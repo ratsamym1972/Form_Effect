f642f5811b2c1fca96147261929abcca39de3906
Ratsamy Maokhamphiou <ratsamym@gmail.com>
2017-10-31 22:34:39 -0700

    second commit

--- js/form_effects.js
+++ js/form_effects.js
@@ -16,6 +16,17 @@ function changeRadio(){
 
 $(document).ready(function(){
      $("form").hide();
-     $("p").click(function(){$("form").toggle(1000);
-return false;});
-})
+     $("p").click(function(){$("form").toggle(1000); return false;});
+     $("input:submit").mousedown(function(){$("form").toggle(1000); return false;});
+     $("textarea").focus(function(){$(this).animate({width:350, height:100}, 1000);});
+     $("textarea").blur(function(){$(this).animate({width:200, height:50}, 1000);});
+     $(".rb").click(changeRadio);
+     $("input:checkbox").prop("checked", false);
+     $(".cb").click(changeCheckbox);
+     $("input:text").keyup(function(){$(this).next("label").html($(this).val());});
+     $("#submit").mouseover(function(){$(this).animate({"background-color":"#0000FF", "width":"140px"},400,"linear");});
+     $("#submit").mouseout(function(){$(this).animate({"background-color":"#3377FF","width":"60px"}, 400, "linear");});
+     $("#submit").focus(function(){$this().animate({"background-color":"#0000FF","border-width":"5px"}, 400, "linear");});
+     $("optgroup").mouseover(function(){$(this).stop(true).animate({"font-size":"18px"}, 200); return false;});
+     $("optgroup").mouseout(function(){$(this).stop(true).animate({"font-size":"15px"}, 200); return false;});
+});
